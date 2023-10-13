f0b32ef7f614f92dfaa7ba06230be2abf1f1d01e
Tiaraalviana <tiaraalviana947@gmail.com>
2023-10-13 15:39:56 +0700

    penambahan file baru

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+        public function index()
+        {
+            echo "<center><h1>Perkenalkan</h1></center>";
+            echo "<br><hr>";
+            echo"Nama saya Tiara Alviana Kusuma Sari <br>
+            <br>Saya tinggal di daerah Bekasi Utara <br>
+            <br>olahraga yang saya sukai adalah Bulutangkis<br>"; 
+ }
+}
